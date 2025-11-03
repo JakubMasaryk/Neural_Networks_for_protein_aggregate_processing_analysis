51807e45d7bf237fb8e02956ddd3160d0984f985
Jakub Masaryk <157375316+JakubMasaryk@users.noreply.github.com>
2025-11-03 14:43:08 +0100

    Update and rename control_sbw_data_clustering.py to clustering-based temporal segmentation of aggregate processing.py

--- clustering-based temporal segmentation of aggregate processing.py
+++ clustering-based temporal segmentation of aggregate processing.py
@@ -1,13 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 # ## __Application of clustering algorithms to set the temporal boundaries of aggregate-processing stages__
 
 # ### __Libraries__
-
-# In[98]:
-
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -24,15 +17,11 @@ from scipy.signal import find_peaks
 # ### __Database Authentication__
 
 # * __fill in__ the authentication parameters
-
-# In[7]:
-
-
 #mysql connection parameters
-username= 'root'
-password= 'poef.qve5353'
-hostname= '127.0.0.1'
-port= '3306'
+username= ''
+password= ''
+hostname= ''
+port= ''
 
 #mysql server connection
 connection_string = f"mysql+pymysql://{username}:{password}@{hostname}:{port}/hc_microscopy_data_v2"
@@ -40,15 +29,10 @@ engine = create_engine(connection_string)
 
 
 # ### __Data Load__
-
 # * data loaded from __'hc_microscopy_data_v2' database__
 # * pre-defined __stored procedure__ 'p_TS_screen_WT_sbw_data'
 # * complete __control__ (WT Hsp104-GFP) __dataset__
 # * __data filtered by__ lower and upper range of __cell counts in the initital timepoint__
-
-# In[10]:
-
-
 def data_load(min_cell_count, max_cell_count, starting_timepoint):
     
     #query to obtain the desired data
@@ -67,10 +51,6 @@ def data_load(min_cell_count, max_cell_count, starting_timepoint):
 
 # * __cell-count range__ for the initital timepoint set to __100-300__ cells
 # * above 300 cell counts tend to overgrow in the later timepoints
-
-# In[12]:
-
-
 data= data_load(100, 300, 4)
 
 
@@ -81,30 +61,14 @@ data= data_load(100, 300, 4)
 # * __multivariete outliers filtered out__ using the __DBSCAN clustering__
 
 # * __data parameters__
-
-# In[16]:
-
-
 data.isna().sum()
 
-
-# In[17]:
-
-
 data.info(memory_usage= 'deep')
-
-
-# In[18]:
-
 
 data.describe()
 
 
 # * __data distributions__
-
-# In[20]:
-
-
 fig, ax= plt.subplots(1, 3, figsize= (19.2, 4.8), sharey= 'all')
 
 ax[0].hist(data.proportion_of_cells_with_agg,
@@ -132,10 +96,6 @@ ax[2].set_title('Average size of a single aggregate', weight= 'bold');
 
 
 # * __normalisation and standardisation__
-
-# In[22]:
-
-
 def _0_1_normalisation(column):
     
     try:
@@ -156,10 +116,6 @@ def standardisation(column):
         print(f'standardization failed: {ex}')
         return column
 
-
-# In[23]:
-
-
 data= data.assign(proportion_of_cells_with_agg_norm= _0_1_normalisation(data.proportion_of_cells_with_agg),
                   proportion_of_cells_with_agg_zscore= standardisation(data.proportion_of_cells_with_agg),
                   avg_number_of_foci_per_cell_norm= _0_1_normalisation(data.avg_number_of_foci_per_cell),
@@ -169,10 +125,6 @@ data= data.assign(proportion_of_cells_with_agg_norm= _0_1_normalisation(data.pro
 
 
 # * __z-score-based univariete outlier removal (-3 to 3)__
-
-# In[25]:
-
-
 def outlier_removal(data):
     try:
         data= data.loc[(data.proportion_of_cells_with_agg_zscore >= -3) & (data.proportion_of_cells_with_agg_zscore <= 3) &
@@ -184,18 +136,10 @@ def outlier_removal(data):
         print(f'data cleaning failed: {ex}')
         return data
 
-
-# In[26]:
-
-
 data= outlier_removal(data)
 
 
 # * __DBSCAN-based multivariete outlier removal__
-
-# In[28]:
-
-
 def dbscan_outlier_detection(data, eps= 0.05, min_samples= 30):
     try:
         dbscan_cleaning= DBSCAN(eps= eps, min_samples= min_samples)
@@ -212,14 +156,7 @@ def dbscan_outlier_detection(data, eps= 0.05, min_samples= 30):
         return data
 
 
-# In[29]:
-
-
 data= dbscan_outlier_detection(data)
-
-
-# In[30]:
-
 
 fig, ax= plt.subplots(1, 3, figsize= (19.2, 4.8), sharex= 'all', sharey= 'all')
 
@@ -252,17 +189,10 @@ ax[2].scatter(data.loc[data.dbscan_outlier==False, 'avg_number_of_foci_per_cell_
 ax[2].set_title('cleaned dataset', weight= 'bold');
 
 
-# In[31]:
-
-
 data= data.loc[data.dbscan_outlier==False]
 
 
 # * __cleared data distribution__
-
-# In[33]:
-
-
 fig, ax= plt.subplots(1, 3, figsize= (19.2, 4.8), sharey= 'all')
 
 ax[0].hist(data.proportion_of_cells_with_agg,
@@ -290,26 +220,17 @@ ax[2].set_title('Average size of a single aggregate', weight= 'bold');
 
 
 # ### __Clustering__
-
 # * __three__ clustering __algorithms__ applied: __K-Means__, __DBSCAN__ and __Gaussian Mixture Model__
 # * for __K-Means__ and __GMM__ the __number of clusters set to 3__
 # * __clustering based on three-dimensional data__: percentage of cells with aggregates, aggregate size and aggregate number
 # * data and clusters __visualized in two-dimensional space__: aggregate size and aggregate number
 
 # * __data__
-
-# In[37]:
-
-
 clustering_data= data.loc[:, ['timepoint_minutes', 'proportion_of_cells_with_agg_norm', 'avg_number_of_foci_per_cell_norm', 'avg_size_single_focus_norm']]
 clustering_data.head()
 
 
 # * __k-means__
-
-# In[39]:
-
-
 def k_means(data, k= 3):
     
     try:
@@ -322,18 +243,10 @@ def k_means(data, k= 3):
         print(f'clustering failed: {ex}')
         return data.assign(k_means_clusters= 0)
 
-
-# In[40]:
-
-
 kmeans_data= k_means(clustering_data)
 
 
 # * __dbscan__
-
-# In[42]:
-
-
 def dbscan(data, epsilon= 0.5, no_of_points= 5):
     
     try:
@@ -346,18 +259,10 @@ def dbscan(data, epsilon= 0.5, no_of_points= 5):
         print(f'clustering failed: {ex}')
         return data.assign(dbscan_clusters= 0)
 
-
-# In[43]:
-
-
 dbscan_data= dbscan(clustering_data, 0.05, 30)
 
 
 # * __gaussian mixture model__
-
-# In[45]:
-
-
 def gmm(data, n_clusters= 3):
     
     try:
@@ -370,14 +275,7 @@ def gmm(data, n_clusters= 3):
         print(f'clustering failed: {ex}')
         return data.assign(gmm_clusters= 0)
 
-
-# In[46]:
-
-
 gmm_data= gmm(clustering_data)
-
-
-# In[47]:
 
 
 fig, ax= plt.subplots(1, 3, figsize= (19.2, 4.8), sharey= 'all', sharex= 'all')
@@ -414,22 +312,11 @@ ax[2].set_title('GMM clustering', weight= 'bold');
 # * __GMM__ selected as __the most suitable__ for temporal boundaries identification
 # * __PCC__ calculated and __RL__ fitted for each of the identified stages (clusters)
 # * identified clusters __re-clustered by DBSCAN to remove multivariete outliers__ (and limit overlaps between the clusters)
-
-# In[50]:
-
-
 gmm_data.head()
 
 
 # * __DBSCAN re-clustering (outlier/cluster-overlap removal)__
-
-# In[52]:
-
-
 gmm_data= gmm_data.groupby('gmm_clusters').apply(lambda x: dbscan_outlier_detection(x, eps= 0.05, min_samples= 40))
-
-
-# In[53]:
 
 
 fig, ax= plt.subplots(1, len(gmm_data.gmm_clusters.unique()), figsize= (6.4*len(gmm_data.gmm_clusters.unique()), 4.8))
@@ -457,17 +344,10 @@ for i, cluster in enumerate(gmm_data.gmm_clusters.unique()):
     ax[i].set_ylabel('average size of a single aggregate (normalized)', weight= 'bold')
 
 
-# In[54]:
-
-
 gmm_data= gmm_data.loc[gmm_data.dbscan_outlier==False]
 
 
 # * __linear regression__
-
-# In[56]:
-
-
 fig, ax= plt.subplots(figsize=(12.8, 9.6))
 
 cmap = plt.cm.get_cmap('Set1')   
@@ -504,18 +384,10 @@ ax.legend(ncol= 3, frameon= False, loc= 'lower right', prop={'weight': 'bold', '
 
 
 # * __timepoints overlap__
-
-# In[158]:
-
-
 gmm_data.head()
 
 
 # >- __histogram peak for each cluster__
-
-# In[228]:
-
-
 def peaks(data, min_height= 100):
     try:
         tmpt_counts= data.groupby('timepoint_minutes')[['timepoint_minutes']].value_counts().reset_index()
@@ -528,10 +400,6 @@ def peaks(data, min_height= 100):
 
 
 # >- __visualisation__
-
-# In[236]:
-
-
 fig, ax= plt.subplots(figsize= (19.2, 4.8))
 
 cmap = plt.cm.get_cmap('Set1')   
@@ -558,9 +426,5 @@ ax.set_xlabel('timepoint (min)', weight= 'bold', fontsize= 12);
 # >>- formation <= Peak 1
 # >>- sequestration > Peak 1 and <= Peak 3
 # >>- clearance > Peak 3
-
-# In[250]:
-
-
 gmm_data.groupby('gmm_clusters').apply(lambda x: peaks(x)).sort_values()
 
