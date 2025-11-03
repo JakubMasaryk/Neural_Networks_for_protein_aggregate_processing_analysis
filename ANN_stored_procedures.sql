3b89ac10de8cc5277d521feceaf8b79097b61e2d
Jakub Masaryk <157375316+JakubMasaryk@users.noreply.github.com>
2025-11-03 08:48:34 +0100

    Update ANN_stored_procedures.sql

--- ANN_stored_procedures.sql
+++ ANN_stored_procedures.sql
@@ -162,6 +162,61 @@ delimiter ;
 
 
 
+-- ANN binary-classification training data (v3)
+-- WT vs. ase1 and num1 mutants
+drop procedure if exists p_ANN_binary_classification_v3;
+delimiter //
+create procedure p_ANN_binary_classification_v3(in p_starting_timepoint int, in p_ending_timepoint int)
+begin
+	with
+	cte_selected_experiments as
+	(
+	select distinct
+		e.date_label,
+		e.microscopy_initial_delay_min,
+		e.microscopy_interval_min
+	from
+		experiments as e
+	inner join
+		strains_and_conditions_main as sacm
+	on
+		e.date_label=sacm.date_label
+	where
+		sacm.mutated_gene_standard_name in ('ASE1', 'NUM1')
+	)
+	select
+		sacm.date_label,
+		sacm.experimental_well_label,
+		fnaa.timepoint * cte1.microscopy_interval_min - (cte1.microscopy_interval_min - cte1.microscopy_initial_delay_min) as timepoint_minutes,
+		fnaa.fov_cell_id,
+		fnaa.number_of_foci,
+		fnaa.total_foci_area/fnaa.number_of_foci as single_focus_avg_area,
+		case
+			when sacm.mutated_gene_standard_name = '-' then 'control' 
+			else 'disrupted_clearance'
+		end as category
+	from
+		strains_and_conditions_main as sacm
+	inner join
+		cte_selected_experiments as cte1
+	on
+		sacm.date_label= cte1.date_label
+	inner join
+		experimental_data_scd_foci_number_and_area as fnaa
+	on
+		sacm.date_label= fnaa.date_label and
+		sacm.experimental_well_label= fnaa.experimental_well_label
+	where
+		sacm.mutated_gene_standard_name in ('-', 'ASE1', 'NUM1') and
+		sacm.metal_concentration > 0 and
+		fnaa.number_of_foci > 0 and
+		fnaa.timepoint * cte1.microscopy_interval_min - (cte1.microscopy_interval_min - cte1.microscopy_initial_delay_min) >= p_starting_timepoint and
+		fnaa.timepoint * cte1.microscopy_interval_min - (cte1.microscopy_interval_min - cte1.microscopy_initial_delay_min) <= p_ending_timepoint;
+end //
+delimiter ;
+
+
+
 -- ANN multiclass-classification training data (v1)
 -- WT vs. cin8', tpm1, tpm2, myo4 and she3 mutants sv. LatA-exposed cells
 drop procedure if exists p_ANN_multi_class_classification_v1;
